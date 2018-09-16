1ad66b3fd5bcc896191ef253714b6c2ebe5a737a
kounger <fabmu92@outlook.de>
2018-09-17 01:31:53 +0200

    Changed the folder structure. Added a program that collects all canonical names. Changed the FileMetadataReader program to collect properties using all canonical names. Refactored the code.

---
+++ FileMetadataReader/CanonicalNameCollector/CanonicalNameCollector.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+using Microsoft.WindowsAPICodePack.Shell.PropertySystem;
+
+namespace CanonicalNameCollector
+{
+    /// <summary>
+    /// This class collects the canonical name of each shell property from 
+    /// the SystemProperties.cs file of the Windows API Code Pack and
+    /// offers a method to serialize them into an XML file. 
+    /// </summary>
+    [XmlRoot(ElementName = "CanonicalNameCollection")]
+    public class CanonicalNameCollector
+    {
+        private List<Type> sysPropsTypes = new List<Type>();
+        [XmlArray("CanonicalNames")]
+        [XmlArrayItem("CanonicalName")]
+        public List<string> canonicalNames = new List<string>();
+
+        public CanonicalNameCollector(Type type)
+        {
+            getSysPropsClasses(type);
+            collectCanonicalNames(sysPropsTypes);
+        }
+
+        public CanonicalNameCollector()
+        {
+        }
+
+        /// <summary>
+        /// This method collects all nested types of the 
+        /// SystemProperties class.
+        /// </summary>
+        public void getSysPropsClasses(Type type)
+        {
+            Type[] nestedTypes = type.GetNestedTypes();
+
+            foreach (Type nestedType in nestedTypes)
+            {
+                sysPropsTypes.Add(nestedType);
+                getSysPropsClasses(nestedType);
+            }
+        }
+
+        /// <summary>
+        /// This method collects the canonical names of all properties
+        /// that are reflected from the given list of types. 
+        /// </summary> 
+        public void collectCanonicalNames(List<Type> types)
+        {
+            foreach (Type type in types)
+            {
+                PropertyInfo[] properties = type.GetProperties();
+
+                foreach (PropertyInfo propInfo in properties)
+                {
+                    PropertyKey key = (PropertyKey)propInfo.GetValue(null);
+                    ShellPropertyDescription desc = SystemProperties.GetPropertyDescription(key);
+                    string canonicalName = desc.CanonicalName;
+
+                    if ((!canonicalNames.Contains(canonicalName)) && canonicalName != null)
+                    {
+                        canonicalNames.Add(canonicalName);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// This method serializes a list of canonical names into 
+        /// an XML file.
+        /// </summary>
+        public void serializeCanonicalNames(string fileName)
+        {
+            var serializer = new XmlSerializer(typeof(CanonicalNameCollector));
+            using (var stream = File.OpenWrite(fileName))
+            {
+                serializer.Serialize(stream, this);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Each shell property has a canonical name as an identifier. This identifier can 
+    /// be used to get a specific property value from a shell container (file or folder).
+    /// This program collects all canonical names and saves them in an XML file. 
+    /// </summary>
+    class CanonicalNameSerializer
+    {
+        static void Main(string[] args)
+        {
+            Type sysPropsType = typeof(SystemProperties);
+            CanonicalNameCollector collector = new CanonicalNameCollector(sysPropsType);
+
+            string savePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\CanonicalNames.xml";
+            collector.serializeCanonicalNames(savePath);
+        }
+    }
+}
---
+++ FileMetadataReader/FileMetadataReader/FileMetadataReader.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using System.Reflection;
+using System.Collections.Generic;
+using Microsoft.WindowsAPICodePack.Shell;
+using Microsoft.WindowsAPICodePack.Shell.PropertySystem;
+
+namespace FileMetadataReader
+{
+    static class FileMetadataReader
+    {
+        static private List<string> canonicalNameList;
+        static private bool restartProgram = false;
+
+        /// <summary>
+        /// This method initializes the program and repeats the main procedure after it finished
+        /// or had to return after an error.
+        /// </summary>
+        public static void run()
+        {
+            Console.WriteLine("################################################################################");
+            Console.WriteLine("############################# FILE METADATA READER #############################");
+            Console.WriteLine("################################################################################");
+
+            readCanonicalNames();
+            while (true) mainProcedure(); 
+        }
+
+        /// <summary>
+        /// Each property has a canonical name which can be used to request the value of a property.
+        /// These canonical names are saved in an xml-file and are loaded into a list during startup.
+        /// </summary>
+        private static void readCanonicalNames()
+        {           
+            Assembly asm = Assembly.GetExecutingAssembly();
+            XDocument xDoc;
+
+            using (Stream stream = asm.GetManifestResourceStream("FileMetadataReader.CanonicalNames.xml"))
+            {
+                xDoc = XDocument.Load(stream);
+            }
+
+            canonicalNameList = xDoc.Descendants("CanonicalName")
+                                    .Select(s => s.Value)
+                                        .ToList();
+        }
+
+        /// <summary>
+        /// This method asks the user for a file path and will return all 
+        /// available file property values inside the console. 
+        /// </summary>
+        private static void mainProcedure()
+        {
+            //If a method sets this flag to true, this mainProcedure will restart:
+            restartProgram = false;
+            //Ask the user to enter a file path:
+            Console.WriteLine();
+            Console.WriteLine("Enter a valid file or folder path:");
+            string filePath = Console.ReadLine();
+            //Check if the file path is valid:
+            checkFilePath(filePath);
+            if (restartProgram) return;
+            //Create a shell object that allows the retrieval of property values:
+            ShellObject item = createShellObject(filePath);
+            if (restartProgram) return;
+            //Retrieve all properties:
+            List<IShellProperty> propertyList = collectProperties(item, canonicalNameList);
+            //Print all properties with their canonical names:
+            printProperties(propertyList);
+        }
+
+        /// <summary>
+        /// This method checks if the given file path is valid. If that is not the case it 
+        /// will set the restartProgram flag to true. 
+        /// </summary>
+        private static void checkFilePath(string filePath)
+        {
+            if (!File.Exists(filePath) && !Directory.Exists(filePath))
+            {
+                Console.WriteLine("This file does not exist. Please enter a path to an existing file or directory.");
+                restartProgram = true;
+            }            
+        }
+
+        /// <summary>
+        /// This method creates a shell object from a file path. This shell object will 
+        /// allow the retrieval of property values.
+        /// </summary>
+        private static ShellObject createShellObject(string filePath)
+        {
+            try
+            {
+                return ShellContainer.FromParsingName(filePath);
+            }
+            catch (ShellException)
+            {
+                Console.WriteLine("Unable to create a ShellObject from this input. Please enter a path to an existing file or directory.");
+                restartProgram = true;
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// This method uses a list of canonical property names to extract all file properties
+        /// whose value isn't null.
+        /// </summary>
+        private static List<IShellProperty> collectProperties(ShellObject shellItem, List<string> canonicalNames)
+        {
+            List<IShellProperty> propertyList = new List<IShellProperty>();
+
+            foreach (string canonicalName in canonicalNames)
+            {
+                IShellProperty property = shellItem.Properties.GetProperty(canonicalName);
+
+                if (property.ValueAsObject != null)
+                {
+                    propertyList.Add(property);
+                }
+            }
+
+            return propertyList;
+        }
+        
+        /// <summary>
+        /// This method formats each property value and prints them inside the
+        /// console with their canonical names.
+        /// </summary>
+        private static void printProperties(List<IShellProperty> propertyList)
+        {
+            int sizeMaxCanName = getLongestCanonicalName(propertyList);
+            int countedProperties = 0;
+
+            //Prints each property value with its canonical name:
+            foreach (IShellProperty property in propertyList)
+            {
+                string canonicalName = property.CanonicalName;
+                string propertyValue = "";
+                countedProperties++;
+
+                try
+                {
+                    //Gets a string representation of the property with a proper format using the installed windows culture:
+                    propertyValue = property.FormatForDisplay(PropertyDescriptionFormatOptions.None);
+                }
+                catch (ShellException)
+                {
+                    //Return the property value as a string if formatting fails and a ShellException was thrown:
+                    propertyValue = getStringPropertyValue(property);
+                }
+
+                //Change the property text value to a proper output format when the text is too long:
+                propertyValue = FileMetadataReader.insertWordWrap(propertyValue, sizeMaxCanName);
+
+                //Print the canonical name with PadRight to have each property text value on the same width:
+                Console.WriteLine(canonicalName.PadRight(sizeMaxCanName + 10, '.') + " " + propertyValue);
+            }
+
+            Console.WriteLine("Counted Items: " + countedProperties);
+        }
+
+        /// <summary>
+        /// This method will return the size of the longest canonical name that will
+        /// be displayed inside the console.
+        /// </summary> 
+        private static int getLongestCanonicalName(List<IShellProperty> propertyList)
+        {
+            return propertyList
+                        .Where(s => s.CanonicalName != null)
+                            .OrderByDescending(s => s.CanonicalName.Length)
+                                .First()
+                                    .CanonicalName.Length;
+        }
+
+        /// <summary>
+        /// This method tries to get the string representation of an property object.
+        /// </summary>
+        private static string getStringPropertyValue(IShellProperty property)
+        {
+            try
+            {                
+                return property.ValueAsObject.ToString();
+            }
+            catch
+            {
+                return "Error: Unable to print this property as a text.";
+            }
+        }
+
+        /// <summary>
+        /// This method formats the text value of a property in a way that a new line 
+        /// and an indention is inserted after every 100 characters.
+        /// </summary>
+        private static string insertWordWrap(string propertyText, int sizeMaxCanName)
+        {
+            if (propertyText.Length >= 100)
+            {
+                string restOfString = propertyText;
+                propertyText = "";
+
+                while (restOfString.Length > 100)
+                {
+                    //propertyText = already processed text + new line + idention + next 100 characters
+                    propertyText = propertyText + restOfString.Substring(0, 100) + "\n" + new string(' ', sizeMaxCanName + 11);
+                    restOfString = restOfString.Substring(100);
+                }
+                propertyText = propertyText + restOfString;
+            }
+
+            return propertyText;
+        }
+
+        static void Main(string[] args)
+        {
+            FileMetadataReader.run();
+        }
+    }
+}
